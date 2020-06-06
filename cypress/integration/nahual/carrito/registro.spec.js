dea1d62641921200b491fdf8400260cceee0e8bc
GomezFacundo <facu_rc23@live.com>
2020-06-06 12:33:46 -0300

    Agrego prueba

---
+++ cypress/integration/nahual/carrito/registro.spec.js
@@ -0,0 +1,8 @@
+/// <reference types="Cypress" />
+describe('', function() {
+    it('', function() {
+      
+    cy.visit ('https://nahual-app-carrito.herokuapp.com/#/store')
+    cy.get('[href="#/registro"]').click()
+    })
+  })
