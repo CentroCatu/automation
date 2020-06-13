b1c524639bf5f01d8bea879e088a153460a0fad5
MauricioIsmael <mauricioismael796@gmail.com>
2020-06-13 12:09:53 -0300

    Agrege un cy.contains nuevo

--- cypress/integration/nahual/carrito/registro.spec.js
+++ cypress/integration/nahual/carrito/registro.spec.js
@@ -4,5 +4,6 @@ describe('', function() {
       
     cy.visit ('https://nahual-app-carrito.herokuapp.com/#/store')
     cy.contains("Registrarse").click()
+    cy.contains("Nombre").click().type(223242)
     })
   }) 
