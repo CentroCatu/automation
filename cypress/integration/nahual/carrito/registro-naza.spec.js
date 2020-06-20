bd9d0ea9a79e35258b22ddfcff90e2ca58440aa2
Nazareno Morillo <nazamorillo1@gmail.com>
2020-06-20 11:59:11 -0300

    agregada la prueba de completar registro, cancelar registro y verificar fecha de nacimiento ingresada

--- cypress/integration/nahual/carrito/registro-naza.spec.js
+++ cypress/integration/nahual/carrito/registro-naza.spec.js
@@ -7,7 +7,21 @@ describe('Funcionalidad de Registro', function() {
         cy.get('[href="#/registro"]')
     })
 
-    it('Verificar campo nombre', function() {
+    it('Completar formulario de registro', function() {
+        cy.visit('https://nahual-app-carrito.herokuapp.com/#/store')
+        cy.get('[href="#/registro"]').click()
+        cy.get('#nombre').type('Anselmo')
+        cy.get('#apellido').type('Valenzuela')
+        cy.get('#registro-email').type('ansel@gmail.com')
+        cy.get('#registro-birthdate').type('2001-08-02')
+        cy.get('#registro-birthdate').should('have.value', '2001-08-02')
+
+        cy.get('#registro-address').type('Siempre viva 123')
+        cy.get('#registro-password').type('1233456')
+        cy.get('#registro-phone').type('3416123456')
+    })
+
+    it('Completar formulario de registro y cancelar', function() {
         cy.visit('https://nahual-app-carrito.herokuapp.com/#/store')
         cy.get('[href="#/registro"]').click()
         cy.get('#nombre').type('Anselmo')
@@ -17,6 +31,26 @@ describe('Funcionalidad de Registro', function() {
         cy.get('#registro-address').type('Siempre viva 123')
         cy.get('#registro-password').type('1233456')
         cy.get('#registro-phone').type('3416123456')
-        cy.contains('Registrarse').click()
+        cy.contains('Cancelar').click()
+    })
+
+    // it('Registro de usuario', function() {
+    //     cy.visit('https://nahual-app-carrito.herokuapp.com/#/store')
+    //     cy.get('[href="#/registro"]').click()
+    //     cy.get('#nombre').type('Anselmo')
+    //     cy.get('#apellido').type('Valenzuela')
+    //     cy.get('#registro-email').type('ansel@gmail.com')
+    //     cy.get('#registro-birthdate').type('2001-08-02')
+    //     cy.get('#registro-address').type('Siempre viva 123')
+    //     cy.get('#registro-password').type('1233456')
+    //     cy.get('#registro-phone').type('3416123456')
+    //     cy.contains('Cancelar').click()
+    // })
+
+    it('Ferificar el valor ingresado en fecha de nacimiento', function() {
+        cy.visit('https://nahual-app-carrito.herokuapp.com/#/store')
+        cy.get('[href="#/registro"]').click()
+        cy.get('#registro-birthdate').type('2001-08-02')
+        cy.get('#registro-birthdate').should('have.value', '2001-08-02')
     })
 })
