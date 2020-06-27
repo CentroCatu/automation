e90c8a0aff20efb812d48ad15ebe472122b1ebf8
GomezFacundo <facu_rc23@live.com>
2020-06-27 10:55:59 -0300

    nuevo test

--- cypress/integration/nahual/carrito/registro.spec.js
+++ cypress/integration/nahual/carrito/registro.spec.js
@@ -1,9 +1,28 @@
 /// <reference types="Cypress" />
 describe('', function() {
-    it('', function() {
+    it( ('CCC1-CCC2 Verificar visualización del botón "Registrarse" y Validar el campo "Nombre" con caracteres Numéricos.'), function() {
       
     cy.visit ('https://nahual-app-carrito.herokuapp.com/#/store')
     cy.contains("Registrarse").click()
-    cy.contains("Nombre").click().type(223242)
-    })
-  }) 
+    cy.contains("Nombre").click().type('223242') 
+    cy.get('#nombre').should('have.value','')  
+  })
+})
+  
+describe('', function() {
+  it( '', function() {
+    cy.visit ('https://nahual-app-carrito.herokuapp.com/#/store')
+    cy.contains("Registrarse").click()    
+    cy.contains("Nombre").click().type('Facundo') 
+    cy.contains("Apellido").click().type('Gomez') 
+  })
+})
+
+
+
+
+
+
+
+
+
