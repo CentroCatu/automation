8b5f5a727fcf3efdc76b117a558c50171e43d690
KhamilN <nazer89@gmail.com>
2020-07-04 11:51:12 -0300

    agregando caso CC-25

--- cypress/integration/nahual/carrito/compra.spec.js
+++ cypress/integration/nahual/carrito/compra.spec.js
@@ -1,25 +1,28 @@
 /// <reference types="Cypress" />
-describe('CCC-4-Verificar busqueda con palabras en campo de texto', function() {
-    it( (''), function() {
-      
-    cy.visit ('https://nahual-app-carrito.herokuapp.com/#/store')
-    cy.get('.ng-pristine').type('Manzana')  
+
+
+
+describe('CCC-4-Verificar busqueda con palabras en campo de texto', function () {
+  it((''), function () {
+
+    cy.visit('https://nahual-app-carrito.herokuapp.com/#/store')
+    cy.get('.ng-pristine').type('Manzana')
     cy.get('tbody > .ng-scope > :nth-child(2)').should('contain', 'Manzana')
   })
-})  
-
-
-describe('CCC-25:Verificar campo costo total', function() {
-    it( (''), function() {
-      
-    cy.visit ('https://nahual-app-carrito.herokuapp.com/#/store')
-    cy.get('.ng-pristine').type('Manzana')  
-    cy.get(':nth-child(4) > a').click()
-    cy.get(':nth-child(4) > a').click()
- 
-    var precio = cy.get('.ng-scope > .tdRight').innerText
-    console.log('valor' + precio ) 
 })
+
+
+describe('CCC-25:Verificar campo costo total', function () {
+  it((''), function () {
+
+    cy.visit('https://nahual-app-carrito.herokuapp.com/#/store')
+    cy.get('.ng-pristine').type('Manzana')
+    cy.get(':nth-child(4) > a').click()
+    cy.get(':nth-child(4) > a').click()
+    cy.get(':nth-child(1) > .tdRight > a > :nth-child(3)').should('have.text', '$80.00')
+    cy.get(':nth-child(3) > .tdRight > a > :nth-child(3)').should('have.text', '$80.00')
+    cy.get('.ng-scope > .tdRight').should('contain', '$40.00 / Kg.')
+  })
 })
 
 
