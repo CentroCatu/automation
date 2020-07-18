ce51bf1a2bbae2addd71470f5f4a5cd6f853783d
Nazareno Morillo <nazamorillo1@gmail.com>
2020-07-18 11:03:19 -0300

    antes de hacer un pull

--- cypress/integration/nahual/carrito/compra.spec.js
+++ cypress/integration/nahual/carrito/compra.spec.js
@@ -1,17 +1,11 @@
 /// <reference types="Cypress" />
-<<<<<<< HEAD
-describe('CCC-4-Verificar busqueda con palabras en campo de texto', function () {
-  it(('Buscar Manzana'), function () {
-=======
-
 beforeEach(() => {
   cy.visit ('https://nahual-app-carrito.herokuapp.com/#/cart')
   cy.get ('.btn-danger').click() 
 })
 
 describe('CCC-4-Verificar busqueda con palabras en campo de texto', function () {
-  it((''), function () {
->>>>>>> 9ad6d8e7dc8cc081ba9f088d14f3cbeb76abf8f5
+  it(('Buscar Manzana'), function () {
 
     cy.visit('https://nahual-app-carrito.herokuapp.com/#/store')
     cy.get('.ng-pristine').type('Manzana')
@@ -19,36 +13,22 @@ describe('CCC-4-Verificar busqueda con palabras en campo de texto', function () 
   })
 })
 
-<<<<<<< HEAD
 
 describe('CCC-25:Verificar campo costo total', function () {
 
   it(('Verificar el precio al sumar 2 Manzanas'), function () {
-=======
-
-describe('CCC-25:Verificar campo costo total', function () {
-  it((''), function () {
->>>>>>> 9ad6d8e7dc8cc081ba9f088d14f3cbeb76abf8f5
 
     cy.visit('https://nahual-app-carrito.herokuapp.com/#/store')
     cy.get('.ng-pristine').type('Manzana')
     cy.get(':nth-child(4) > a').click()
     cy.get(':nth-child(4) > a').click()
-<<<<<<< HEAD
 
     cy.get(':nth-child(1) > .tdRight > a > :nth-child(3)').should("have.text", "$80.00")
     cy.get(':nth-child(3) > .tdRight > a > :nth-child(3)').should("have.text", "$80.00")
     cy.get('.ng-scope > .tdRight').should("contain", "$40.00 / Kg.")
   })
 
-=======
-    cy.get(':nth-child(1) > .tdRight > a > :nth-child(3)').should('have.text', '$80.00')
-    cy.get(':nth-child(3) > .tdRight > a > :nth-child(3)').should('have.text', '$80.00')
-    cy.get('.ng-scope > .tdRight').should('contain', '$40.00 / Kg.')
-  })
->>>>>>> 9ad6d8e7dc8cc081ba9f088d14f3cbeb76abf8f5
 })
-
 
 describe('CCC-30:Visualizar columnas Khamil', function () {
   it((''), function () {
@@ -62,4 +42,3 @@ describe('CCC-30:Visualizar columnas Khamil', function () {
     cy.get(':nth-child(1) > .tdRight > b').should('have.text', 'Precio')
   })
 })
-
