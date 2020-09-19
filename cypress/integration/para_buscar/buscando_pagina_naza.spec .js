f911510abecfe74394629310b249a16cf3c4ae08
Nazareno Morillo <nazamorillo1@gmail.com>
2020-09-19 12:42:54 -0300

    mi index.html para probar

--- cypress/integration/para_buscar/buscando_pagina_naza.spec .js
+++ cypress/integration/para_buscar/buscando_pagina_naza.spec .js
@@ -1,5 +1,7 @@
 /// <reference types="Cypress" />
 // node_modules/.bin/cypress open
+// Comando para forzar la apertura de cypress desde una terminal windows
+// npx cypress open
 
 describe('Ejercicios Para buscar elementos', function() {
 
@@ -7,10 +9,10 @@ describe('Ejercicios Para buscar elementos', function() {
         cy.visit('http://127.0.0.1:8887/')        
     })
 
-    it('Seleccionar en el elemento testing manual en la sección de Serviciós', function() {
-        cy.contains('testing manual').parent()
+    it.only('Seleccionar en el elemento testing manual en la sección de Serviciós', function() {
+        cy.get('h1').contains('Servicios').closest('.section').contains('testing manual')
     })
-    
+
     it('Hacer click en el elemento Precio de páinas web en la sección de Serviciós', function() {
         cy.contains('páginas web').parent().contains('Precio').click()
     })
