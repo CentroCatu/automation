7430bdd2ad987ae1a3a5aa8a73dc57388aac1213
Nazareno Morillo <nazamorillo1@gmail.com>
2020-12-19 12:24:29 -0300

    prueba de juego Naza

--- prueba.js
+++ prueba.js
@@ -1,14 +1,33 @@
-var mundo=["M"," "," ","S"," "," "," "," ","H"," "," "," ","Y"," "," "];
+var mundo=["🎄"," "," ","🚗"," "," "," "," ","🎄"," "," "," ","🎄"," "," "];
 var posicion = 3;
 console.log(mundo);
-var personaje = "😜";
-var nuevoMundo = mundo.map(function(celda, numCelda){
-    if (posicion == numCelda) {
-        return personaje
-        
-    } else {
-        return celda
-        
-    }
+var personaje = "🧟";
+
+function renderizar(pos){
+    var nuevoMundo = mundo.map(function(celda, numCelda){
+        if (pos == numCelda) {
+            return personaje
+        } else {
+            return celda
+        }
 })
-console.log(nuevoMundo);
+console.log(nuevoMundo); 
+}
+
+
+var flechaIzquierda = "ArrowLeft"
+var flechaDerecha = "ArrowRight"
+
+
+document.onkeydown = function(e) {
+    console.clear()
+    if(e.key== flechaIzquierda){
+        posicion--
+    }else if(e.key== flechaDerecha){
+        posicion++
+    }
+    //console.log(e.key)
+    console.log(posicion)
+    renderizar(posicion)
+    
+}
