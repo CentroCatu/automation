8f5a33219e1f0620b1536edf744f6ab843613d50
carolinanoelsosa <carolinanoelsosa@gmail.com>
2020-12-19 11:44:25 -0300

    aaa

--- prueba.js
+++ prueba.js
@@ -1,5 +1,14 @@
-function aMayuscula(unNombre){
-    return unNombre.toUpperCase();
-}
-let texto=aMayuscula("nazareno");
-console.log(texto);
+var mundo=["M"," "," ","S"," "," "," "," ","H"," "," "," ","Y"," "," "];
+var posicion = 3;
+console.log(mundo);
+var personaje = "😜";
+var nuevoMundo = mundo.map(function(celda, numCelda){
+    if (posicion == numCelda) {
+        return personaje
+        
+    } else {
+        return celda
+        
+    }
+})
+console.log(nuevoMundo);
