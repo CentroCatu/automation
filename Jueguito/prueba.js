2d16f84f46bdda9c4a3c668dc9b72714e01b3126
alejandro-panza <alejandrofpanza@gmail.com>
2021-03-20 12:51:14 -0300

    la gallina va...

--- Jueguito/prueba.js
+++ Jueguito/prueba.js
@@ -1,31 +1,58 @@
 var posicionPersonajeFila = 1;
 var posicionPersonajeColumna = 0;
 var mundo2d = [
-    ["🚗", " ", " ", " ", "🚗", " ", " ", " ", "🚗", " ", " ", " ", "🚗", " ", " "], 
-    [" ", " ", "🚗", " ", " ", " ", "🚗", " ", " ", " ", "🚗", " ", " ", " ", "🚗"]
-]
+  [
+    "🚗",
+    " ",
+    " ",
+    " ",
+    "🚗",
+    " ",
+    " ",
+    " ",
+    "🚗",
+    " ",
+    " ",
+    " ",
+    "🚗",
+    " ",
+    " ",
+  ],
+  [
+    " ",
+    " ",
+    "🚗",
+    " ",
+    " ",
+    " ",
+    "🚗",
+    " ",
+    " ",
+    " ",
+    "🚗",
+    " ",
+    " ",
+    " ",
+    "🚗",
+  ],
+];
 var personaje = "🐓";
 
 document.onkeydown = function (e) {
-
-    if (e.key == "ArrowUp") {
-
-        posicionPersonaje = posicionPersonaje + 1;
-
-    } else {
-
-        posicionPersonaje = posicionPersonaje - 1;
-    
-    }
-    actualizarMundo()
+  if (e.key == "ArrowUp") {
+    posicionPersonajeFila = posicionPersonajeFila - 1;
+  } else if (e.key == "ArrowDown") {
+    posicionPersonajeFila = posicionPersonajeFila + 1;
+  } else if (e.key == "ArrowLeft") {
+    posicionPersonajeColumna = posicionPersonajeColumna - 1;
+  } else if (e.key == "ArrowRight") {
+    posicionPersonajeColumna = posicionPersonajeColumna + 1;
+  }
+  actualizarMundo();
 };
-
-
 
 var nuevoMundo = [];
 // //setInterval(function () {
-
-
 
 //     var nuevoMundo = mundo.map(function (celda, numCelda) {
 //         if (posicionPersonaje == numCelda) {
@@ -37,7 +64,6 @@ var nuevoMundo = [];
 //         }
 //     })
 
-
 //     //console.log(nuevoMundo)
 //     console.clear()
 //     console.table(mundo2d)
@@ -48,50 +74,36 @@ var nuevoMundo = [];
 
 //     }
 
-
 // }, 50)
 
-function actualizarMundo(){
-    console.clear()
-    if (posicionPersonajeColumna == 0){
-        var nuevoMundo = mundo2d[0].map(function (celda, numCelda) {
-            if (posicionPersonajeFila == numCelda) {
-                return personaje
-    
-            } else {
-                return celda
-    
-            }
-        })
+function actualizarMundo() {
+  console.clear();
+  var nuevoMundoPrimera = mundo2d[0];
+  var nuevoMundoSegunda = mundo2d[1];
 
-    } else{
-        var nuevoMundo = mundo2d[1].map(function (celda, numCelda) {
-            if (posicionPersonajeFila == numCelda) {
-                return personaje
-    
-            } else {
-                return celda
-    
-            }
-        })
+  if (posicionPersonajeFila == 0) {
+    nuevoMundoPrimera = mundo2d[0].map(function (celda, numCelda) {
+      if (posicionPersonajeColumna == numCelda) {
+        return personaje;
+      } else {
+        return celda;
+      }
+    });
+  } else {
+    var nuevoMundoSegunda = mundo2d[1].map(function (celda, numCelda) {
+      if (posicionPersonajeColumna == numCelda) {
+        return personaje;
+      } else {
+        return celda;
+      }
+    });
+  }
+  var nuevoMundo = [nuevoMundoPrimera, nuevoMundoSegunda];
+  console.table(nuevoMundo);
 
-    }
-    var nuevoMundo = mundo.map(function (celda, numCelda) {
-        if (posicionPersonaje == numCelda) {
-            return personaje
+  // if (mundo[posicionPersonaje] == "🚗") {
+  //     console.log('PUM!')
+  //     personaje = '👼'
 
-        } else {
-            return celda
-
-        }
-    })
-
-    console.table(mundo2d)
-
-    if (mundo[posicionPersonaje] == "🚗") {
-        console.log('PUM!')
-        personaje = '👼'
-
-    }
-
+  // }
 }
